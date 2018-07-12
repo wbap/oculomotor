0daa4d814b0264ec8ccd104c21cf9d134d05249c
Kotone Itaya <kotone@sfc.keio.ac.jp>
2018-07-12 13:57:50 +0900

    Add minimal server-client integration

--- docker/server/app.py
+++ docker/server/app.py
@@ -1,5 +1,7 @@
-import json
 from http import HTTPStatus
+
+import numpy as np
+
 import flask
 from flask import Flask, request
 app = Flask(__name__)
@@ -12,11 +14,24 @@ class Space(object):
         pass
 
     def sample(self):
-        return 0
+        dx = np.random.uniform(low=-0.02, high=0.02)
+        dy = np.random.uniform(low=-0.02, high=0.02)
+        return np.array([dx, dy])
 
 
 space = Space()
 agent = Agent(space)
+
+
+def npEncode(obj):
+    return {
+        'data': obj.tolist(),
+        'type': obj.dtype.str,
+    }
+
+
+def npDecode(obj):
+    return np.array(obj['data'], dtype=np.dtype(obj['type']))
 
 
 @app.route('/initialize', methods=['GET'])
@@ -27,11 +42,11 @@ def reset():
 
 @app.route('/step', methods=['POST'])
 def step():
-    if request.headers['Context-Type'] != 'application/json':
+    if request.headers['Content-Type'] != 'application/json':
         return 'expected JSON body', HTTPStatus.BAD_REQUEST
-    observation, reward, done, _ = request.json
-    action = agent(observation, reward, done)
-    return flask.jsonify(action), HTTPStatus.OK
+    obs, reward, done, _ = request.json
+    action = agent(npDecode(obs), reward, done)
+    return flask.jsonify(npEncode(action)), HTTPStatus.OK
 
 
 if __name__ == '__main__':
