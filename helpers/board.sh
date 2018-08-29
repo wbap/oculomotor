e940b4edef8cc35fb28534a3b8edb38ff0ce2ba9
Kotone Itaya <kotone@sfc.keio.ac.jp>
2018-08-29 14:22:13 +0900

    Remove entrypoint from tensorboard helper

--- helpers/board.sh
+++ helpers/board.sh
@@ -2,4 +2,4 @@
 
 CONTAINER_APP=/opt/oculomotor
 
-docker run -it -p 6006:6006 --rm -v ${PWD}:${CONTAINER_APP} wbap/oculomotor tensorboard --logdir=${CONTAINER_APP}/log
+docker run -it -p 6006:6006 --rm -v ${PWD}:${CONTAINER_APP} --entrypoint "" wbap/oculomotor tensorboard --logdir=${CONTAINER_APP}/log
